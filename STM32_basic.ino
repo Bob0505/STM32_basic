33fc708e8b165e71884813f6b74355c7c6e1b989
bob_CKL <bob-0505@hotmail.com>
2017-11-25 15:07:07 +0800

    Add GPIO/UART Hello World.

---
+++ STM32_basic.ino
@@ -0,0 +1,29 @@
+/**
+ * STM32F103C8T6 basic code
+ *
+ * @author Bob Chen (bob-0505@gotmail.com)
+ * @date 1 November 2017
+ * https://github.com/Bob0505/E32-TTL-100
+ */
+
+#define LED_PIN           PC13
+#define SERIAL_BAUD       115200
+
+//The setup function is called once at startup of the sketch
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(SERIAL_BAUD);
+  Serial.println("STM32 - Setup");
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+  Serial.println("STM32 - loop");
+
+  digitalWrite(LED_PIN, HIGH);	// turn the LED on (HIGH is the voltage level)
+  delay(500);
+  digitalWrite(LED_PIN, LOW);	// turn the LED off by making the voltage LOW
+  delay(500);
+}
