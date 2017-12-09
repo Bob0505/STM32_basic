9c9db44d34f5db566a55d0d9f830b03173e312dc
bob_CKL <bob-0505@hotmail.com>
2017-12-09 16:27:32 +0800

    Add SPI OLED sample. Pins mapping as bellow. +==========================+ | STM32F103C8T6 | SPI_OLED | +==========================+ |      GND      |   GND    | |      VCC      |   3.3    | |      SCL      | STM32_A5 | |      SDA      | STM32_A7 | |      RST      | STM32_A3 | |      D/C      | STM32_A2 | ===========================+

--- .ino.cpp
+++ .ino.cpp
@@ -2,9 +2,25 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-12-09 15:11:52
+//This file has been generated on 2017-12-09 16:16:52
 
 #include "Arduino.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) ;
+void testdrawchar(void) ;
+void testdrawcircle(void) ;
+void testfillrect(void) ;
+void testdrawtriangle(void) ;
+void testfilltriangle(void) ;
+void testdrawroundrect(void) ;
+void testfillroundrect(void) ;
+void testdrawrect(void) ;
+void testdrawline() ;
+void testscrolltext(void) ;
+void oled_setup()   ;
 void setup() ;
 void loop() ;
 
