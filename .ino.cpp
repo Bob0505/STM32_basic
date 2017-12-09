3d21763623eb0e971db5e7e5ad59f3001684b840
bob_CKL <bob-0505@hotmail.com>
2017-12-09 16:49:07 +0800

    Add timer lib.

--- .ino.cpp
+++ .ino.cpp
@@ -2,9 +2,12 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2017-12-09 15:11:52
+//This file has been generated on 2017-12-09 16:47:18
 
 #include "Arduino.h"
+#include <Timer.h>
+void UpdateLed() ;
+void task_500ms() ;
 void setup() ;
 void loop() ;
 
--- STM32_basic.ino
+++ STM32_basic.ino
@@ -5,9 +5,27 @@
  * @date 25 November 2017
  * https://github.com/Bob0505/E32-TTL-100
  */
+//https://github.com/JChristensen/Timer
+#include <Timer.h>
 
 #define LED_PIN           PC13
 #define SERIAL_BAUD       115200
+
+//Timer Task_1s;
+Timer Task_500ms;
+
+void UpdateLed()
+{
+  static bool LED_Status;
+
+  digitalWrite(LED_PIN, LED_Status);	// turn the LED off by making the voltage HIGH
+  LED_Status = !LED_Status;
+}
+
+void task_500ms()
+{
+	UpdateLed();
+}
 
 //The setup function is called once at startup of the sketch
 void setup()
@@ -15,15 +33,14 @@ void setup()
   pinMode(LED_PIN, OUTPUT);
   Serial.begin(SERIAL_BAUD);
   Serial.println("STM32 - Setup");
+
+  Task_500ms.every(150, task_500ms);
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
   Serial.println("STM32 - loop");
+  Task_500ms.update();
 
-  digitalWrite(LED_PIN, HIGH);	// turn the LED off by making the voltage HIGH
-  delay(1000);
-  digitalWrite(LED_PIN, LOW);	// turn the LED on  by making the voltage LOW
-  delay(500);
 }
